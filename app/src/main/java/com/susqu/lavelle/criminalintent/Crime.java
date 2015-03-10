98d39508c8b729e3a66d83fae4ff804cd1225c07
Shannon Lavelle <lavelles@susqu.edu>
2015-03-10 17:33:11 -0400

    Challenge: More Dialogs

--- app/src/main/java/com/susqu/lavelle/criminalintent/Crime.java
+++ app/src/main/java/com/susqu/lavelle/criminalintent/Crime.java
@@ -8,7 +8,7 @@ import java.util.UUID;
  */
 public class Crime {
     private UUID mId;
-    private String mTitle;
+    private String mTitle = "New Crime";
     private Date mDate;
     private boolean mSolved;
 
